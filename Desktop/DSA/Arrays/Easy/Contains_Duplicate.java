fb0cb1001f02fbfd98d46243b27f81bfca1308f0
Praneeth903 <saipraneeth903@gmail.com>
2026-08-28 02:34:42 +0530

    Problems added

---
+++ Desktop/DSA/Arrays/Easy/Contains_Duplicate.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        // HashMap <Integer,Integer> map = new HashMap<>();
+        
+        // for(int i=0;i<nums.length;i++){
+        //     if(map.containsKey(nums[i])){
+        //         return true;
+        //     }
+        //     else{
+        //         map.put(nums[i],1);
+        //     }
+        // }
+        // return false;
+        HashSet<Integer> set = new HashSet<>();
+        for(int i:nums){
+            if(set.contains(i)){
+                return true;
+            }
+            set.add(i);
+        }
+        return false;
+    }
+}
